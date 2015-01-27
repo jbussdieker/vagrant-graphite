aa5e43a760e1f80b68a9ec2f8e8dbc8a44d97293
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-01-27 01:35:55 -0800

    40-50k messages/sec on c3.large

--- bin/generate_load.rb
+++ bin/generate_load.rb
@@ -2,19 +2,27 @@
 require 'socket'
 require 'time'
 
-def generate_metric(namespace="foo.")
-  random1 = (rand * 2000).to_i
-  random2 = (rand * 2000).to_i
-  timestamp = Time.now.to_i
-  "#{namespace}test-#{random1} #{random2} #{timestamp}"
-end
-
 def socket
   @socket ||= TCPSocket.new ARGV[0], ARGV[1]
 end
 
-def run_test(interval, count, duration)
-  mps = (1.0 / interval) * count
+def generate_metric(mps)
+  @metric_id ||= 0
+
+  if @metric_id >= 4000 # Make sure we have enough id's to match our message per second times the size of our smallest metric
+  #if @metric_id >= mps # Make sure we have enough id's to match our message per second times the size of our smallest metric
+    @metric_id = 1 
+  else
+    @metric_id += 1
+  end
+
+  random = (rand * 2000).to_i
+  timestamp = Time.now.to_i
+  "krux.test-#{@metric_id} #{random} #{timestamp}"
+end
+
+def run_test(mps, count, duration = nil)
+  interval = 1.0 / (mps / count)
   testid = (rand * 10000).to_i
 
   puts "#{Time.now} #{mps.to_i} messages/sec"
@@ -23,7 +31,7 @@ def run_test(interval, count, duration)
   start = Time.now
 
   while true do
-    if Time.now - start > duration
+    if duration && (Time.now - start > duration)
       puts
       return
     end
@@ -31,16 +39,19 @@ def run_test(interval, count, duration)
     if Time.now - last > interval
       metrics = []
       count.times do
-        metrics << generate_metric("foo.")
+        metrics << generate_metric(mps)
       end
+      puts metrics.join("\n")
       socket.puts metrics.join("\n")
       #print "."
       last = Time.now
+    else
+      sleep(interval - (Time.now - last))
     end
   end
 end
 
-run_test(ARGV[2].to_f, ARGV[3].to_i, 60)
+run_test(ARGV[2].to_f, ARGV[3].to_i)
 
 #run_test(0.05, 20, 60)
 #sleep 10
